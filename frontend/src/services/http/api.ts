813c7decfa76571a30f1d904c720a8b59b0824c4
Ezequiel Dominguez Santos <eze.dmgz06@gmail.com>
2026-05-25 14:35:22 -0300

    feat: axios interceptor

--- frontend/src/services/http/api.ts
+++ frontend/src/services/http/api.ts
@@ -1,10 +1,22 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8080';
+const baseURL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8080/api/v1';
 
 export const api = axios.create({
   baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('@ELixo:token');
+  
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
