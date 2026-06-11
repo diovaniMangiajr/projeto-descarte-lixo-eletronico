df6386eda6d8ae601acd2d8e253245784dc8b904
Ezequiel Dominguez Santos <eze.dmgz06@gmail.com>
2026-06-11 12:22:04 -0300

    ajustes no repositorio

--- src/app/App.tsx
+++
@@ -1,5 +0,0 @@
-import { AppRoutes } from '@/app/routes/AppRoutes';
-
-export default function App() {
-  return <AppRoutes />;
-}
--- src/app/routes/paths.ts
+++
@@ -1,8 +0,0 @@
-export const AppPaths = {
-  home: '/',
-  login: '/login',
-  admin: '/admin',
-  mapa: '/mapa',
-} as const;
-
-export type AppPath = (typeof AppPaths)[keyof typeof AppPaths];
